cab17bf8d28f2c466d84e6ed0513666b094bcbc5
jhsheng <jhsheng@mail.ustc.edu.cn>
2024-10-17 15:51:48 +0800

    Add helper macros

--- util.h
+++ util.h
@@ -1,6 +1,29 @@
 #ifndef util_h
 #define util_h
 #include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
+/* Assert-like macros */
+#define TEST(x)                                                                                                        \
+    do {                                                                                                               \
+        if (!(x)) {                                                                                                    \
+            fprintf(stderr, "ERROR: '%s' failed\n", #x);                                                               \
+            fprintf(stderr, "ERROR: at %s:%d\n", __FILE__, __LINE__);                                                  \
+            exit(1);                                                                                                   \
+        }                                                                                                              \
+    } while (0)
+
+#define TRY(x)                                                                                                         \
+    do {                                                                                                               \
+        int __rc = (x);                                                                                                \
+        if (__rc < 0) {                                                                                                \
+            fprintf(stderr, "ERROR: TRY(%s) failed\n", #x);                                                            \
+            fprintf(stderr, "ERROR: at %s:%d\n", __FILE__, __LINE__);                                                  \
+            fprintf(stderr, "ERROR: rc=%d errno=%d (%s)\n", __rc, errno, strerror(errno));                             \
+            exit(1);                                                                                                   \
+        }                                                                                                              \
+    } while (0)
+
 uint64_t hton64(uint64_t value);
 uint64_t ntoh64(uint64_t value);
 #endif
