692d496a70d2d73bee854c65545b709e319b39cb
jhsheng <jhsheng@mail.ustc.edu.cn>
2024-10-17 17:43:17 +0800

    slave: extract and print the timestamp

--- slave.c
+++ slave.c
@@ -92,6 +92,92 @@ static void echo(int sock, unsigned char* buf, int buflen, struct configuration*
     }
 }
 
+/* Processing */
+static void print_time(struct timespec* ts)
+{
+    if (ts == NULL) {
+        printf("no timestamp\n");
+        return;
+    }
+    /* Hardware timestamping provides three timestamps -
+     *   system (software)
+     *   transformed (hw converted to sw)
+     *   raw (hardware)
+     * in that order - though depending on socket option, you may have 0 in
+     * some of them.
+     */
+    // printf("timestamps " TIME_FMT TIME_FMT TIME_FMT "\n",
+    //   (uint64_t)ts[0].tv_sec, (uint64_t)ts[0].tv_nsec,
+    //   (uint64_t)ts[1].tv_sec, (uint64_t)ts[1].tv_nsec,
+    //   (uint64_t)ts[2].tv_sec, (uint64_t)ts[2].tv_nsec );
+
+    struct timeval time_user;
+    gettimeofday(&time_user, NULL);
+    // printf("time_user : %d.%06d\n", (int) time_user.tv_sec,
+    //                                 (int) time_user.tv_usec);
+
+    static uint64_t diff_nic_kernel = 0;
+    static uint64_t diff_nic_user = 0;
+    static uint64_t diff_kernel_user = 0;
+    static int64_t nic_kernel_total_diff = 0;
+    uint64_t old_diff_nic_kernel = diff_nic_kernel;
+
+    uint64_t nanoseconds_nic = ts[2].tv_sec * 1000000000 + ts[2].tv_nsec;
+    uint64_t nanoseconds_kernel = ts[0].tv_sec * 1000000000 + ts[0].tv_nsec;
+    uint64_t nanoseconds_user = time_user.tv_sec * 1000000000 + time_user.tv_usec * 1000;
+
+    printf("nic: %ld, kernel: %ld, user: %ld\n", nanoseconds_nic,
+           nanoseconds_kernel, nanoseconds_user);
+
+    diff_nic_kernel = (ts[0].tv_sec - ts[2].tv_sec) * 1000000000 + (ts[0].tv_nsec - ts[2].tv_nsec);
+
+    // nic_kernel_latency_numbers[total_received++] = diff_nic_kernel; // all latency numbers are in nanoseconds
+
+    if (old_diff_nic_kernel != 0) {
+        nic_kernel_total_diff += diff_nic_kernel - old_diff_nic_kernel;
+    }
+    diff_kernel_user = (time_user.tv_sec - ts[0].tv_sec) * 1000000000 + (time_user.tv_usec * 1000 - ts[0].tv_nsec);
+    diff_nic_user = (time_user.tv_sec - ts[2].tv_sec) * 1000000000 + (time_user.tv_usec * 1000 - ts[2].tv_nsec);
+
+    // nic_user_latency_numbers[total_received] = diff_nic_user; // all latency numbers are in nanoseconds
+    // kernel_user_latency_numbers[total_received] = diff_kernel_user; // all latency numbers are in nanoseconds
+
+    // printf("Kernel timestamp %lds %ldns\n", ts[0].tv_sec, ts[0].tv_nsec);
+    // printf("Kernel timestamp %lds %ldns\n", ts[2].tv_sec, ts[2].tv_nsec);
+
+    // printf("Difference NIC->Kernel: %ld, change of diff_nic_kernel: %ld, at
+    // %ld\n", diff_nic_kernel, diff_nic_kernel-old_diff_nic_kernel,
+    // nic_kernel_total_diff);
+
+    // printf("Difference NIC->User: %ld\n", diff_nic_user);
+
+    // printf("Difference Kernel->User: %ld\n", diff_kernel_user);
+}
+
+/* Given a packet, extract the timestamp(s) */
+static void handle_time(struct msghdr* msg, struct configuration* cfg)
+{
+    struct timespec* ts = NULL;
+    struct cmsghdr* cmsg;
+
+    for (cmsg = CMSG_FIRSTHDR(msg); cmsg; cmsg = CMSG_NXTHDR(msg, cmsg)) {
+        if (cmsg->cmsg_level != SOL_SOCKET)
+            continue;
+
+        switch (cmsg->cmsg_type) {
+        case SO_TIMESTAMPNS:
+            ts = (struct timespec*)CMSG_DATA(cmsg);
+            break;
+        case SO_TIMESTAMPING:
+            ts = (struct timespec*)CMSG_DATA(cmsg);
+            break;
+        default:
+            /* Ignore other cmsg options */
+            break;
+        }
+    }
+
+    print_time(ts);
 }
 
 static int do_recv(int sock, struct configuration* cfg)
