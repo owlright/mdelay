b85ca9d275e05a586e9a02cc1fcb2e4d23961ef0
jhsheng <jhsheng@mail.ustc.edu.cn>
2024-10-21 17:16:08 +0800

    Add delay message types to mdelayhdr.h

--- mdelayhdr.h
+++ mdelayhdr.h
@@ -2,8 +2,14 @@
 #define mdelayhdr_h
 #include <stdint.h>
 
+#define DELAY_REQ 0
+#define DELAY_REQ_FOLLOW_UP 1
+#define DELAY_RESP 2
+#define DELAY_RESP_FOLLOW_UP 3
+
 struct mdelayhdr {
     uint32_t seq;
+    uint8_t type;
     uint64_t t1;
     uint64_t t2;
     uint64_t t3;
